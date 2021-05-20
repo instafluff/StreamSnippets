c4fc803e4d78cbece0c119d2489ec25fa67c70a5
instafluff <instaflufftv@gmail.com>
2021-05-20 12:40:01 -0700

    WORKS!

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
+**StreamSnippets** Stream Overlay to Display Top Twitch Clips for any stream using Twitch commands!
+
 # StreamSnippets
-Stream Overlay for Twitch Clip Highlights
+We built this Stream Overlay for Twitch Clip Highlights live on Twitch for The Comfy Corner!
+
+## Instafluff ##
+> *Like these projects? The best way to support my open-source projects is by becoming a Comfy Sponsor on GitHub!*
+
+> https://github.com/sponsors/instafluff
+
+> *Come and hang out with us at the Comfiest Corner on Twitch!*
+
+> https://twitch.tv/instafluff
+
+## Instructions ##
+
+Add this as a 1280x720 Browser Source and replace `yourchannel` with your Twitch username!
+
+[https://www.instafluff.tv/StreamSnippets?channel=yourchannel](https://www.instafluff.tv/StreamSnippets?channel=YOURCHANNEL)
+
+```
+For example:
+https://www.instafluff.tv/StreamSnippets?channel=instafluff
+```
+
+And then use any of the following chat commands:
+- **!topclip (channel) [number of clips to show]** - Shows a weighted-random top clip from the channel
+- **!showclip (link or ID)** - Show specific clip
+- **!hideclip** - Stop the clip
+- (with `chat=true` enabled) **Any Twitch clip link in Chat** - Shows the clip if it is shared by the Broadcaster or Moderator
+
+```
+For example:
+!topclip instafluff 5
+!showclip https://clips.twitch.tv/CarelessPricklyMomEagleEye
+```
+
+## Optional Parameters ##
+
+- Detect Clip Link in Chat `chat=true` (Broadcaster & Mods Only)
+
+```
+For example:
+https://www.instafluff.tv/StreamSnippets?channel=instafluff&chat=true
+```
+
+## Credits ##
+
+Special Thanks to circular17 for the help with the weighted random index picker!
+
+Thank you too all the participants of this project!
+
+****
---
+++ web/clips.js
@@ -0,0 +1,92 @@
+
+let showCount = 5;
+let topClips = [];
+
+async function TopTeamClips( team, count = 5 ) {
+	// TODO: Pick a random member from the team and show clips
+
+}
+
+async function TopClips( channel, count = 5 ) {
+	const data = await fetch( `https://api.twitch.tv/kraken/clips/top?channel=${channel}&period=all&limit=100`, {
+		headers: {
+			"Accept": "application/vnd.twitchtv.v5+json",
+			"Client-ID": "2odsv8xermvalbub7wipebrphqlpqv"
+		}
+	} ).then( r => r.json() );
+	topClips = data.clips;
+	// console.log( topClips );
+
+	// Pick Random and SetClip
+	showCount = count;
+	SetRandomTopClip();
+}
+
+function SetRandomTopClip() {
+	if( topClips.length > 0 ) {
+		// Pick a weighted-random one
+		let sum = 0;
+		let thresholds = [ 0 ];
+		for( let i = 0; i < topClips.length; i++ ) {
+			sum += Math.pow( 1.05, i );
+			thresholds.push( sum );
+		}
+		let num = sum * Math.random();
+		let index = 0;
+		for( let i = 0; i < topClips.length; i++ ) {
+			if( thresholds[ i ] <= num && num <= thresholds[ i + 1 ] ) {
+				index = topClips.length - i - 1;
+				break;
+			}
+		}
+		// console.log( index, num, sum, topClips.length )
+		// // Pick a random one
+		// let index = Math.floor( topClips.length * Math.random() );
+		let clip = topClips[ index ];
+		// console.log( "picked", index );
+		topClips.splice( index, 1 );
+		// console.log( clip );
+		SetClip( true, clip.slug );
+	}
+}
+
+async function SetClip( visible, id = "" ) {
+	if( visible ) {
+		let clipInfo = await fetch( `https://api.twitch.tv/kraken/clips/${id}`, {
+			headers: {
+				"Accept": "application/vnd.twitchtv.v5+json",
+				"Client-ID": "2odsv8xermvalbub7wipebrphqlpqv"
+			}
+		} ).then( r => r.json() );
+		// console.log( clipInfo );
+		let title = clipInfo.title;
+		let credit = clipInfo.curator.display_name;
+		let creditPhoto = clipInfo.curator.logo;
+		// document.getElementById( "div-credit" ).style.display = "block";
+		// document.getElementById( "clip-title" ).innerText = `"${title}"`;
+		// if( credit !== "Instafluff" ) {
+		//     document.getElementById( "clip-author" ).innerText = `by ${credit}`;
+		// }
+		if( clipInfo && clipInfo.slug ) {
+			setTimeout( () => {
+				if( showCount > 0 ) {
+					SetRandomTopClip();
+				}
+				else {
+					// Auto-Hide
+					SetClip( false, "" );
+				}
+			}, clipInfo.duration * 1000 );
+		}
+		var clip = document.querySelector( "#fluffy-clip" );
+		clip.setAttribute( "src", "https://clips.twitch.tv/embed?parent=localhost&tt_medium=clips_api&tt_content=embed&autoplay=true&clip=" + id );
+		clip.style.display = "block";
+		showCount--;
+	}
+	else {
+		var clip = document.querySelector( "#fluffy-clip" );
+		clip.setAttribute( "src", "" );
+		clip.style.display = "none";
+		// document.getElementById( "div-credit" ).style.display = "none";
+	}
+}
